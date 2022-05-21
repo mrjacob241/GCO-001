aabd89854c794fdf696fb9e69d68695be56bd31f
mrjacob241 <52462865+mrjacob241@users.noreply.github.com>
2022-05-21 08:52:29 +0200

    first commit

---
+++ test_H/test_H.ino
@@ -0,0 +1,30 @@
+#define L 3
+#define R 4
+
+void setup() {
+  pinMode(L, OUTPUT);
+  pinMode(R, OUTPUT); 
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+int cc=1;
+void loop() {
+  if (cc==1) {
+    digitalWrite(L, HIGH); //3
+    digitalWrite(R, LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+  } else {
+    digitalWrite(L, LOW);
+    digitalWrite(R, HIGH); //4
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(350);
+     
+    digitalWrite(L, LOW);
+    digitalWrite(R, LOW);
+    delay(300);
+  }
+  cc = -1*cc;
+  
+  
+}
---
+++ test_PWM/test_PWM.ino
@@ -0,0 +1,44 @@
+#define L 3
+#define R 4
+#define pot A0
+
+int cc=1;
+float frac = 0.9;
+int interval = 50;
+int TP = L;
+int val = 0;
+
+void setup() {
+  pinMode(L, OUTPUT);
+  pinMode(R, OUTPUT); 
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+    val = analogRead(A0);
+    frac = 0.25+0.75*(1.0*val)/700;
+    if (frac < 0.29) { frac = 0.0; }
+
+    if (frac < 0.975) {
+    Serial.print(val);
+    Serial.print(" | ");
+    Serial.println(frac);
+    digitalWrite(TP, HIGH); //3
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(int(frac*interval));
+    digitalWrite(TP, LOW); //3
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(int((1-frac)*interval));
+    } else {
+    Serial.print(val);
+    Serial.print(" | ");
+    Serial.println(frac);
+    digitalWrite(TP, HIGH); //3
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(interval); 
+    }
+ 
+  
+  
+}
